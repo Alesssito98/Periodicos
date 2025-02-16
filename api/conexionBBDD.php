584a334a211ae6d908904d1a9e37db43c1c7dc20
Alesssito98 <alexgaggero159@gmail.com>
2025-02-16 20:55:32 +0100

    me quiero morir

--- api/conexionBBDD.php
+++ api/conexionBBDD.php
@@ -2,13 +2,9 @@
 
 $Repit = false;
 
-// Obtener la cadena de conexión desde las variables de entorno
-$conn_string = getenv("DB_CONNECTION_STRING");
 
-// Verificar si la variable de entorno está definida
-if (!$conn_string) {
-    die("Error: La variable de entorno DB_CONNECTION_STRING no está definida.");
-}
+// Cadena de conexión
+$conn_string = "postgres://default:DlC2mcJFxIr4@ep-yellow-dust-a7l7gm6q.ap-southeast-2.aws.neon.tech:5432/verceldb?sslmode=require";
 
 // Conectar a PostgreSQL
 $link = pg_connect($conn_string);
@@ -20,7 +16,16 @@ if (!$link) {
 // Configurar codificación de caracteres a UTF8
 pg_set_client_encoding($link, "UTF8");
 
-// Puedes eliminar este mensaje en producción
-// echo "Conexión a PostgreSQL exitosa.";
+echo "Conexión a PostgreSQL exitosa.";
 
+// Aquí puedes ejecutar tus consultas SQL
+
+// Ejemplo: Ejecutar una consulta
+// $result = pg_query($link, "SELECT * FROM tu_tabla");
+// while ($row = pg_fetch_assoc($result)) {
+//     echo $row['nombre_columna'];
+// }
+
+// Cierra la conexión cuando termines
+// pg_close($link);
 ?>
