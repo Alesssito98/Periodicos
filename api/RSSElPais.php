f4f218d474701ab65a3de0417101e6ecc655db84
Alesssito98 <alexgaggero159@gmail.com>
2025-02-16 21:24:26 +0100

    Ya me puedo morir

--- api/RSSElPais.php
+++ api/RSSElPais.php
@@ -2,28 +2,23 @@
 
 require_once "conexionRSS.php";
 
-// Descargar el XML del feed
 $sXML = download("https://feeds.elpais.com/mrss-s/pages/ep/site/elpais.com/portada");
 $oXML = new SimpleXMLElement($sXML);
 
-// Conectar a PostgreSQL
 require_once "conexionBBDD.php";
 
-// Preparar la consulta de inserción con parámetros
 $sqlInsert = "INSERT INTO elpais (titulo, link, descripcion, categoria, fpubli, contenido) VALUES ($1, $2, $3, $4, $5, $6) ON CONFLICT (link) DO NOTHING";
 
-// Iniciar transacción
 pg_query($link, "BEGIN");
 
-$insertCount = 0; // Contador de inserciones
-$valuesArray = []; // Array para almacenar los parámetros de las consultas
+$insertCount = 0;
+$valuesArray = [];
 
 foreach ($oXML->channel->item as $item) {
     if ($insertCount >= 5) {
-        break; // Detener el procesamiento después de 40 inserciones
+        break;
     }
 
-    // Filtrar categorías
     $categoriaFiltro = '';
     foreach ($item->category as $category) {
         if (in_array($category, ["Política", "Deportes", "Ciencia", "España", "Economía", "Música", "Cine", "Europa", "Justicia"])) {
@@ -31,14 +26,12 @@ foreach ($oXML->channel->item as $item) {
         }
     }
 
-    // Formatear la fecha
     $fPubli = strtotime($item->pubDate);
     $new_fPubli = date('Y-m-d', $fPubli);
 
     $content = $item->children("content", true);
     $encoded = (string)$content->encoded;
 
-    // Preparar los valores para la inserción
     $valuesArray[] = [
         (string)$item->title,
         (string)$item->link,
@@ -48,22 +41,16 @@ foreach ($oXML->channel->item as $item) {
         $encoded
     ];
 
-    $insertCount++; // Incrementar el contador de inserciones
+    $insertCount++;
 }
 
-// Insertar en bloque si hay valores
 if (count($valuesArray) > 0) {
     foreach ($valuesArray as $values) {
         pg_query_params($link, $sqlInsert, $values);
     }
 }
 
-// Confirmar la transacción
 pg_query($link, "COMMIT");
-
-// Cerrar la conexión a la base de datos
-#pg_close($link);
-
 ?>
 
 
--- api/conexionBBDD.php
+++ api/conexionBBDD.php
@@ -2,30 +2,16 @@
 
 $Repit = false;
 
-
-// Cadena de conexión
 $conn_string = "postgres://default:DlC2mcJFxIr4@ep-yellow-dust-a7l7gm6q.ap-southeast-2.aws.neon.tech:5432/verceldb?sslmode=require";
 
-// Conectar a PostgreSQL
 $link = pg_connect($conn_string);
 
 if (!$link) {
     die("Error en la conexión: " . pg_last_error());
 }
 
-// Configurar codificación de caracteres a UTF8
 pg_set_client_encoding($link, "UTF8");
 
 echo "Conexión a PostgreSQL exitosa.";
 
-// Aquí puedes ejecutar tus consultas SQL
-
-// Ejemplo: Ejecutar una consulta
-// $result = pg_query($link, "SELECT * FROM tu_tabla");
-// while ($row = pg_fetch_assoc($result)) {
-//     echo $row['nombre_columna'];
-// }
-
-// Cierra la conexión cuando termines
-// pg_close($link);
 ?>
